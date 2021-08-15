d2938ec58ee5c22c2dc7e7365662e2084a8c46e5
Hector <hector@git.com>
2021-08-15 13:08:22 -0500

    primer proyecto en git

--- main.js
+++ main.js
@@ -3,13 +3,15 @@ const typed = new Typed('.typed', {
 		'<i class="mascota">Sueño...</i>',
 		'<i class="mascota">Deseo...</i>',
 		'<i class="mascota">Futuro...</i>',
-		'<i class="mascota">Camino...</i>'
+		'<i class="mascota">Camino...</i>',
+		'<i class="mascota">Mañana...</i>',
+		'<i class="mascota">Diseño...</i>'
 	],
 
 	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
 	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	backSpeed: 80, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
